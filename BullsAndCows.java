58779d1e9ad20ee9c7ada43ddd8c5bde8f2f40cd
Debadrita Debnath <debadrita1517@gmail.com>
2020-11-12 06:27:41 +0530

    Update BullsAndCows.java

--- BullsAndCows.java
+++ BullsAndCows.java
@@ -5,7 +5,6 @@ The number of "bulls", which are digits in the guess that are in the correct pos
 The number of "cows", which are digits in the guess that are in your secret number but are located in the wrong position. Specifically, the non-bull digits in the guess that could be rearranged such that they become bulls.
 Given the secret number secret and your friend's guess guess, return the hint for your friend's guess.
 The hint should be formatted as "xAyB", where x is the number of bulls and y is the number of cows. Note that both secret and guess may contain duplicate digits.
-
 Example 1:
 Input: secret = "1807", guess = "7810"
 Output: "1A3B"
@@ -13,7 +12,6 @@ Explanation: Bulls are connected with a '|' and cows are underlined:
 "1807"
   |
 "7810"
-
 Example 2:
 Input: secret = "1123", guess = "0111"
 Output: "1A1B"
@@ -22,16 +20,13 @@ Explanation: Bulls are connected with a '|' and cows are underlined:
   |      or     |
 "0111"        "0111"
 Note that only one of the two unmatched 1s is counted as a cow since the non-bull digits can only be rearranged to allow one 1 to be a bull.
-
 Example 3:
 Input: secret = "1", guess = "0"
 Output: "0A0B"
-
 Example 4:
 Input: secret = "1", guess = "1"
 Output: "1A0B"
- 
-Constraints:
+ Constraints:
 1 <= secret.length, guess.length <= 1000
 secret.length == guess.length
 secret and guess consist of digits only.

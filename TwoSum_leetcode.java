0af7d6574e7fce0265b21b9ad6709ea5add73093
Debadrita Debnath <debadrita1517@gmail.com>
2020-12-21 17:53:20 +0530

    Create TwoSum_leetcode.java

---
+++ TwoSum_leetcode.java
@@ -0,0 +1,46 @@
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]*/
+
+class Solution
+{
+    public int[] twoSum(int[] arr,int k)
+    {
+        int[] res=new int[2];
+        HashSet<Integer> set=new HashSet<Integer>();
+        for(int i=0;i<arr.length;i++)
+            set.add(arr[i]);
+        int digit2=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(set.contains(k-arr[i]))
+            {
+                if((k-arr[i])!=arr[i])
+                {
+                    res[0]=i;
+                    digit2=k-arr[i];
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==digit2)
+            {
+                res[1]=i;
+                break;
+            }
+        }
+        return res;
+    }
+}
